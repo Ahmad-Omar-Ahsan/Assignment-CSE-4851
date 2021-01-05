d0a4973f3fddb7610241040957a33e039f3a33b1
Ahmad Omar Ahsan <gigablaze662@gmail.com>
2021-01-05 17:01:59 +0600

    Added basic shapes

---
+++ Shapes/Circle.java
@@ -0,0 +1,8 @@
+package Shapes;
+import java.util.ArrayList;
+
+public class Circle extends BaseShape{
+    public Circle(String name, String material, String color){
+        super(name, material, color, new ArrayList<>());
+    }
+}
